2247ff61969ef990bc80e9ca8f3885d836c3eb73
Arnav goel <arnav.dtu2020@gmail.com>
2018-05-29 21:23:01 +0530

    Initial Commit
    Freeboard initials and step by step procedutre for the clouding the sensor values over the web server

---
+++ DHT22_sensor_iot/DHT22_sensor_iot.ino
@@ -0,0 +1,87 @@
+// Import required libraries
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <ESP8266WiFi.h>
+// WiFi parameters
+char* ssid = "Palak Goel";
+char* password = "7005374551";
+
+float temp;
+float humidity;
+
+#define DHTTYPE DHT22   //DHT TYPE
+
+//GPIO2  Pin as input
+#define DHTPIN D7       
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Host
+const char* host = "dweet.io";
+
+void setup() {
+  
+  // Start Serial
+  Serial.begin(115200);
+  delay(10);
+  dht.begin();
+
+  //  start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+ 
+  Serial.print("Connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // Reading temperature and humidity
+    humidity= dht.readHumidity();
+    temp= dht.readTemperature();
+   if (isnan(humidity) || isnan(temp)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return; }
+  
+  // This will send the request to the server
+  client.print(String("GET /dweet/for/arnavesp8266?temperature=") + String(temp) +"&humidity=" + String(humidity)+" HTTP/1.1\r\n" +"Host: " + host + "\r\n" + "Connection: close\r\n\r\n");
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+  
+  // Repeat every 10 seconds
+  delay(10000);
+ 
+}
+
+
+
